54caf7869a08164699bde71bbffbac4bacb75775
julher-2 <julher-2@student.ltu.se>
2025-10-20 16:29:57 +0200

    changed client file

--- ground.py
+++ ground.py
@@ -1,5 +1,29 @@
 import socket
 import time
+from datetime import datetime
+
+
+
+def Open_file(header, client_socket):
+
+	if header.startswith("FILE:"):
+		filesize = int(header.split(":")[1])
+		client_socket.send(b"READY")  # acknowledge readiness
+
+		received = 0
+		with open("received_data.txt", "wb") as f:
+			while received < filesize:
+				data = client_socket.recv(1024)
+				if not data:
+					break
+				f.write(data)
+				received += len(data)
+
+		print(f"File received ({received} bytes) and saved as 'received_data.txt'.")
+
+	else:
+		print("Received from SC", header)
+
 
 def client_program():
 	host = socket.gethostname()  # as both code is running on same pc
@@ -8,37 +32,23 @@ def client_program():
 	client_socket = socket.socket()  # instantiate
 	client_socket.connect((host, port))  # connect to the server
 
-	
+
 	message = input(" -> ")  # take input
 
 	while True:
 		client_socket.send(message.encode())  # send message
 
+		header = client_socket.recv(1024).decode()
+
 		if message.lower().strip() == "bye":
 			print("Sent bye, closing...")
 			time.sleep(0.2)
 			break
+		elif message.lower().strip() =="payload":
+			Open_file(header, client_socket)
+			print("Messaged recieved at:", datetime.now())
 
 
-		header = client_socket.recv(1024).decode()
-
-		if header.startswith("FILE:"):
-			filesize = int(header.split(":")[1])
-			client_socket.send(b"READY")  # acknowledge readiness
-
-			received = 0
-			with open("received_data.txt", "wb") as f:
-				while received < filesize:
-					data = client_socket.recv(1024)
-					if not data:
-						break
-					f.write(data)
-					received += len(data)
-
-			print(f"File received ({received} bytes) and saved as 'received_data.txt'.")
-
-		else:
-			print("Received from SC", header)
 
 
 		message = input(" -> ")  # again take input
--- payload.py
+++ payload.py
@@ -4,6 +4,8 @@ import numpy as np
 import random
 import os
 import time
+from datetime import datetime
+
 
 def heartbeat():
 	return random.choices(population=[1,0], weights=[0.9,0.1], k=1)
@@ -18,7 +20,7 @@ def heartbeatcheck(stop_event):
 			a = 0
 		else:
 			a +=1
-			if a>=3:
+			if a>=5:
 				print("No heartbeat detected, offline")
 				break
 		time.sleep(0.5)
@@ -27,12 +29,11 @@ def heartbeatcheck(stop_event):
 def randomdata():
 	datapoints = random.randint(0,100)
 	print(datapoints)
-	data = np.zeros((datapoints,3))
+	data = np.zeros((datapoints,4))
 	for i in range(0,datapoints) :
 		data[i][0] = random.randint(0,100)
 		data[i][1] = random.randint(0,100)
 		data[i][2] = random.randint(0,100)
-
 	return data
 
 def write_to_txt(data, filename="data.txt"):
@@ -42,10 +43,16 @@ def write_to_txt(data, filename="data.txt"):
 def send_payload(conn):
 		random_array = randomdata()
 		filename = "data.txt"
-		filesize = os.path.getsize(filename)
 		write_to_txt(random_array, filename)
+		filesize = os.path.getsize(filename)
+
 
 		conn.sendall(f"FILE:{filesize}".encode())
+
+		ack = conn.recv(1024).decode()
+		if ack.strip().upper() != "READY":
+			print("Client not ready, aborting send.")
+			return
 
 		with open(filename, "r") as f:
 			file_data = f.read()
@@ -62,6 +69,7 @@ def server_program(stop_event):
 
 	server_socket = socket.socket()  # get instance
     # look closely. The bind() function takes tuple as argument
+	server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	server_socket.bind((host, port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
@@ -72,7 +80,6 @@ def server_program(stop_event):
 	while True:
         # receive data stream. it won't accept data packet greater than 1024 bytes
 		data = conn.recv(1024)
-
 		if not data:
             		break
 
